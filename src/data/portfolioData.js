e146fa4143c67d43f64d2a3931fcea6f7f29c84d
steno1 <onuprinceley@gmail.com>
2024-08-03 13:17:04 +0100

    added solar pv calculator

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,16 +1,17 @@
 import ecommerce from "../asset/eCommerce.jpg"
 import portfolio from "../asset/portfolio.jpg"
+import solarCalculator from "../asset/solarCalculator.PNG"
 import task from "../asset/task.jpg"
 
 const portfolioData = [
     {
       id: 1,
-      category: "Frontend",
-      image: portfolio,
-      title: "My Portfolio Site (Frontend)",
-      description: "A frontend-focused portfolio site built using React, Context API, HTML, and CSS. It includes sections like Header, Home, About, Portfolio, Projects, etc",
-      demo: "https://steno1.github.io/lastPortfolio/",
-      githubLink: "https://github.com/steno1/lastPortfolio/tree/master"
+      category: "MERN stack",
+      image: solarCalculator,
+      title: "Solar PV System Sizing Calculator (MERN)",
+      description: "A Solar PV Calculator built with the MERN stack, offering load analysis and component sizing for solar panels, batteries, charge controllers, and inverters, complete with user authentication and state management",
+      demo: "https://solar-calculator-1.onrender.com/",
+      githubLink: "https://github.com/steno1/solar_calculator"
     },
     {
       id: 2,
@@ -29,7 +30,16 @@ const portfolioData = [
       description: "An eCommerce platform built with React, Redux, and integrated PayPal. Includes features like rating and comment sections, search functionalities, and more.",
       demo: "https://stenosupermarket1.onrender.com/",
       githubLink: "https://github.com/steno1/eCommerceRedone"
-    }
+    },
+    {
+      id: 4,
+      category: "Frontend",
+      image: portfolio,
+      title: "My Portfolio Site (Frontend)",
+      description: "A frontend-focused portfolio site built using React, Context API, HTML, and CSS. It includes sections like Header, Home, About, Portfolio, Projects, etc",
+      demo: "https://steno1.github.io/lastPortfolio/",
+      githubLink: "https://github.com/steno1/lastPortfolio/tree/master"
+    },
   ];
   
   export default portfolioData;
